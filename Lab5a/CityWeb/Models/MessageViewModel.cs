fdec7f44562cc1a990a857e3d9c89c59647e1bb7
Tony Plueard <ptony25@gmail.com>
2016-02-18 21:31:53 -0800

    Lab 5 final

---
+++ Lab5a/CityWeb/Models/MessageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CityWeb.Models
+{
+    public class MessageViewModel
+    {
+        public int MessageID { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Body { get; set; }
+        public string Date { get; set; }
+        
+        public string From { get; set; }
+        public Topic TopicName { get; set; }
+        //public Member UserName { get; set; }
+    }
+}
